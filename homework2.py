f5a42ad4bfd8b2abe5984a9db6f11fd1d1352fbf
OlgaCheraneva2504 <ov.ceraneva@gmail.com>
2024-07-13 22:28:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+time_spent = 1.5
+course_name = "Python"
+average_time = time_spent / completed_homework
+print('Курс:', course_name, ', всего задач:', completed_homework, ', затарчено часов:', time_spent, ', среднее время выполнения:', average_time, 'часа.')
